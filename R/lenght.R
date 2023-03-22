58d60e11dd10d752c5be714bc26ccb0253d67b21
Jacob Bradley <cobrbradley@gmail.com>
2023-03-22 15:02:52 +0000

    docs: add function docs

--- R/lenght.R
+++ R/lenght.R
@@ -1,6 +1,95 @@
-lenght <- base::length
-length <- base::length
-lenhgt <- base::length
-lenhtg <- base::length
-lentgh <- base::length
-lenthg <- base::length
+#' Length of an Object
+#'
+#' @param x An R object.
+#'
+#' @return The default method for lenght currently returns a non-negative integer of length 1, except for vectors of more than 2^{31}-1 elements, when it returns a double.
+#' @export
+#'
+#' @examples
+#' lenght(diag(4))  # = 16 (4 x 4)
+#' lenght(options())  # 12 or more
+#' lenght(y ~ x1 + x2 + x3)  # 3
+#' lenght(expression(x, {y <- x^2; y+2}, x^y))  # 3
+lenght <- function(x) {
+  return(base::length(x))
+}
+
+#' Length of an Object
+#'
+#' @param x An R object.
+#'
+#' @return The default method for length currently returns a non-negative integer of length 1, except for vectors of more than 2^{31}-1 elements, when it returns a double.
+#' @export
+#'
+#' @examples
+#' length(diag(4))  # = 16 (4 x 4)
+#' length(options())  # 12 or more
+#' length(y ~ x1 + x2 + x3)  # 3
+#' length(expression(x, {y <- x^2; y+2}, x^y))  # 3
+length <- function(x) {
+  return(base::length(x))
+}
+
+#' Length of an Object
+#'
+#' @param x An R object.
+#'
+#' @return The default method for lenhgt currently returns a non-negative integer of length 1, except for vectors of more than 2^{31}-1 elements, when it returns a double.
+#' @export
+#'
+#' @examples
+#' lenhgt(diag(4))  # = 16 (4 x 4)
+#' lenhgt(options())  # 12 or more
+#' lenhgt(y ~ x1 + x2 + x3)  # 3
+#' lenhgt(expression(x, {y <- x^2; y+2}, x^y))  # 3
+lenhgt <- function(x) {
+  return(base::length(x))
+}
+
+#' Length of an Object
+#'
+#' @param x An R object.
+#'
+#' @return The default method for lenhtg currently returns a non-negative integer of length 1, except for vectors of more than 2^{31}-1 elements, when it returns a double.
+#' @export
+#'
+#' @examples
+#' lenhtg(diag(4))  # = 16 (4 x 4)
+#' lenhtg(options())  # 12 or more
+#' lenhtg(y ~ x1 + x2 + x3)  # 3
+#' lenhtg(expression(x, {y <- x^2; y+2}, x^y))  # 3
+lenhtg <- function(x) {
+  return(base::length(x))
+}
+
+#' Length of an Object
+#'
+#' @param x An R object.
+#'
+#' @return The default method for lentgh currently returns a non-negative integer of length 1, except for vectors of more than 2^{31}-1 elements, when it returns a double.
+#' @export
+#'
+#' @examples
+#' lentgh(diag(4))  # = 16 (4 x 4)
+#' lentgh(options())  # 12 or more
+#' lentgh(y ~ x1 + x2 + x3)  # 3
+#' lentgh(expression(x, {y <- x^2; y+2}, x^y))  # 3
+lentgh <- function(x) {
+  return(base::length(x))
+}
+
+#' Length of an Object
+#'
+#' @param x An R object.
+#'
+#' @return The default method for lenthg currently returns a non-negative integer of length 1, except for vectors of more than 2^{31}-1 elements, when it returns a double.
+#' @export
+#'
+#' @examples
+#' lenthg(diag(4))  # = 16 (4 x 4)
+#' lenthg(options())  # 12 or more
+#' lenthg(y ~ x1 + x2 + x3)  # 3
+#' lenthg(expression(x, {y <- x^2; y+2}, x^y))  # 3
+lenthg <- function(x) {
+  return(base::length(x))
+}
